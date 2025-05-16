98dcef8d713781372fa564cd31443ac177f22489
barlow21 <gerrit.boeing@gmx.de>
2025-05-16 23:44:35 +0200

    more pages

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import re
 import shutil
 from markdown_blocks import extract_title, markdown_to_html_node
-from textnode import TextNode, TextType
 
 
 def copy_directory(src, dest):
@@ -24,9 +23,7 @@ def copy_static_content(static_dir, public_dir):
     copy_directory(static_dir, public_dir)
 
 
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-
+def generate_page(from_path, dest_path, template_path):
     markdown = Path(from_path).read_text()
     output = Path(template_path).read_text()
     content = markdown_to_html_node(markdown).to_html()
@@ -38,16 +35,26 @@ def generate_page(from_path, template_path, dest_path):
     Path(dest_path).write_text(output)
 
 
+def generate_pages(src, dest, template_path):
+    for name in os.listdir(src):
+        fullname = os.path.join(src, name)
+        dest_name = os.path.join(dest, name)
+        if os.path.isfile(fullname):
+            generate_page(fullname, dest_name.replace(".md", ".html"), template_path)
+        else:
+            os.mkdir(dest_name)
+            generate_pages(fullname, dest_name, template_path)
+
+
 def main():
     cwd = os.getcwd()
     static_dir = os.path.join(cwd, "static")
     public_dir = os.path.join(cwd, "public")
     copy_static_content(static_dir, public_dir)
 
-    index_md = os.path.join(cwd, "content", "index.md")
-    target = os.path.join(public_dir, "index.html")
-    template = os.path.join(cwd, "template.html")
-    generate_page(index_md, template, target)
+    content_dir = os.path.join(cwd, "content")
+    template_path = os.path.join(cwd, "template.html")
+    generate_pages(content_dir, public_dir, template_path)
 
 
 main()
